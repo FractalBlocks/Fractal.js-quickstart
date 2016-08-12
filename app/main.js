a076a696492a9bf66f69ecb7d2529456258f803d
Carlos Galarza <carloslfu@gmail.com>
2016-08-12 10:10:55 -0500

    Updated Fractal.js dependency

--- app/main.js
+++ app/main.js
@@ -5,26 +5,39 @@ const h = F.h
 module.exports = F.def({
   init: ({key}) => ({
     key,
-    toggleState: true,
+    isActive: false,
   }),
   inputs: {
     toggle: (ctx, Action, _) => Action.Toggle(_),
   },
   actions: {
-    Toggle: [[R.T], (_, m) => R.evolve({toggleState: R.not}, m)],
+    Toggle: [[R.T], (_, m) => R.evolve({isActive: R.not}, m)],
   },
   interfaces: {
-    view: (ctx, i, m) => h('div', [
+    view: (ctx, i, m) => h('div', { key: m.key, class: { [styles.base]: true } }, [
       h('div', {
-        style: {
-          backgroundColor: (m.toggleState) ? 'purple' : 'green',
-          width: '100px',
-          height: '100px',
+        class: {
+          [styles.button.base]: true,
+          [styles.button.active]: m.isActive,
         },
         on: {
           click: i.toggle,
         },
       }),
     ]),
+  },
+})
+
+let styles = F.css.rs({
+  base: {},
+  button: {
+    base: {
+      backgroundColor: 'purple',
+      width: '100px',
+      height: '100px',
+    },
+    active: {
+      backgroundColor: 'green',
+    },
   },
 })
